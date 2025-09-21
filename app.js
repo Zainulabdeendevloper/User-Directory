4dee1958f080b899f6a6f91c05dbeded336d2bb8
Zainulabdeendevloper <bhuttozain141@gmail.com>
2025-09-21 16:11:36 +0500

    Initial commit for new project

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require(`express`) ;
 const app = express() ;
 const path = require(`path`)
-const userModel  = require(`./models/user`)
+const userModel  = require(`./models/user`);
+const user = require("./models/user");
 
 app.set("view engine" ,"ejs")
 app.use(express.json()) ;
@@ -12,10 +13,41 @@ app.get(`/` , (req , res) => {
 res.render("index")
 } ) ;
 
+app.get("/delete/:id", async (req, res) => {
+  try {
+    await userModel.findByIdAndDelete(req.params.id);  // delete by id
+    res.redirect("/read");  // back to users list
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+
+app.get("/edit/:userid", async (req, res) => {
+  let user = await  userModel.findOne({_id: req.params.userid})
+  res.render("edit", {user})
+   
+});
+
+app.post("/update/:userid", async (req, res) => {
+  let { name, email, image } = req.body;
+  await userModel.findOneAndUpdate(
+    { _id: req.params.userid },
+    { name, email, image },
+    { new: true }
+  );
+  res.redirect("/read");
+});
+
+
+
+
+
 app.get("/read", async (req, res) => {
   try {
-    let users = await userModel.find();   // MongoDB se saare users le raha hai
-    res.render("read", { users });        // EJS ko "users" bhejna zaroori hai
+    let users = await userModel.find(); 
+   userModel.find().sort({ _id: -1 }) 
+    res.render("read", { users });        
   } catch (err) {
     res.send(err);
   }
@@ -23,16 +55,23 @@ app.get("/read", async (req, res) => {
 
 
 
-app.post(`/Create` ,async (req , res) => {
-    let { name , email , image} = req.body ;
-let createduser = await userModel.create({
-    name, 
-    email, 
-    image
-})
+app.post("/Create", async (req, res) => {
+  let { name, email, image } = req.body;
 
-res.send(createduser) 
-})
+  // agar image empty ya galat hai, ek default use karo
+  if (!image || !image.startsWith("http")) {
+    image = "https://i.pravatar.cc/150?u=" + email; // random avatar based on email
+  }
+
+  let createdUser = await userModel.create({
+    name,
+    email,
+    image
+  });
+
+  res.redirect("/read");
+});
+
 
 
 
--- models/user.js
+++ models/user.js
@@ -1,14 +1,12 @@
-const mongoose = require("mongoose");
+const { name } = require("ejs");
+const mongoose = require(`mongoose`)
 
-// Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/database");
+mongoose.connect( "mongodb://127.0.0.1:27017/database") ;
 
-// Define schema
-const userSchema = new mongoose.Schema({
-  image: String,
-  email: String,
-  name: String
-});
+const userSchema = mongoose.Schema( {
+    image  : String ,
+    email : String , 
+    name :  String 
+}) ;
 
-// Export model
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model(`user` , userSchema ) 
