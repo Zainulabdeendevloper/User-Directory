5cd234e78b0db3f35add3737bf22764543479f62
Zain-ul-Abdeen <bhuttozain141@gmail.com>
2025-09-21 23:57:25 +0500

    Update app.js
    changed the port

--- app.js
+++ app.js
@@ -75,4 +75,8 @@ app.post("/Create", async (req, res) => {
 
 
 
-app.listen(3000) ;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
